6e71d56ffc927dba33cf45bbd39a0eeafdde90b7
Jerome Chan <eviltofu@mac.com>
2020-06-24 11:49:26 +0800

    Added documentation strings and renamed nil-node to reset-node.

--- src/doubly-linked-list.lisp
+++ src/doubly-linked-list.lisp
@@ -1,3 +1,5 @@
+;;;; A doubly-linked-list implementation.
+
 (in-package #:starfish)
 
 ;;; Global variables
@@ -23,40 +25,60 @@
 
 (defclass node ()
   ((previous-node
+    :type node
     :initarg :previous-node
     :initform nil
-    :accessor previous-node)
+    :accessor previous-node
+    :documentation "The previous node. Nil indicates no previous node.")
    (next-node
+    :type node
     :initarg :next-node
     :initform nil
-    :accessor next-node)
+    :accessor next-node
+    :documentation "The next node. Nil indicates no next node.")
    (value
     :initarg :value
-    :accessor value)))
+    :accessor value
+    :documentation "The value stored in the node. Can be anything."))
+  (:documentation "A node of a doubly-linked-list."))
 
 (defclass linked-list ()
   ((head
+    :type node
     :initform nil
-    :reader head)
+    :reader head
+    :documentation "The head of the list. Nil means no head.")
    (tail
+    :type node
     :initform nil
-    :reader tail)
+    :reader tail
+    :documentation "The tail of the list. Nil means no tail.")
    (size
+    :type non-negative-integer-type
     :initarg :size
-    :reader linked-list-size)
+    :reader linked-list-size
+    :documentation "How many nodes can the linked list contain.")
    (length
+    :type non-negative-integer-type
     :initform 0
-    :reader linked-list-length)))
+    :reader linked-list-length
+    :documentation "How many nodes the linked list actually contains."))
+  (:documentation "A doubly-linked-list."))
 
 ;;; Public functions
 
 (declaim (ftype (function (&key (:size positive-integer-type)) linked-list) make-linked-list))
 (defun make-linked-list (&key (size *default-maximum-list-size*))
+  "Make a linked list. Size determines how many nodes can be in the list. If no size is given, a default size is used."
   (let ((created-linked-list (make-instance 'linked-list :size size)))
     created-linked-list))
 
 (declaim (ftype (function (linked-list t &optional linked-list-position-type) linked-list) add-value))
 (defun add-value (linked-list value &optional (position :rear))
+  "Adds something to the linked list.
+You can specify which end of the list the value is added to via :front or :rear symbols.
+If you specify an unknown symbol, the function will signal an unknown-position-error.
+If the list is at maximum capacity (length == size), the function will signal a linked-list-full-error."
   (cond
     ((linked-list-full-p linked-list)
      (error 'linked-list-full-error :linked-list linked-list))
@@ -70,6 +92,9 @@
 
 (declaim (ftype (function (linked-list &optional linked-list-position-type) t) remove-value))
 (defun remove-value (linked-list &optional (position :front))
+  "Remove something from the linked list. 
+You can specify which end of the list the value is removed from via :front or :rear symbols.
+If the list is empty, linked-list-empty-error is signaled. "
   (cond
     ((linked-list-empty-p linked-list)
      (error 'linked-list-empty-error :linked-list linked-list))
@@ -83,11 +108,14 @@
 
 (declaim (ftype (function (linked-list) (or null t)) linked-list-empty-p))
 (defun linked-list-empty-p (linked-list)
+  "Is the linked list empty?"
   (with-slots (length) linked-list
     (eq 0 length)))
 
+
 (declaim (ftype (function (linked-list) (or null t)) linked-list-full-p))
 (defun linked-list-full-p (linked-list)
+  "Is the linked list at maximum capacity?"
   (with-slots (length size) linked-list
     (eq length size)))
 
@@ -95,6 +123,7 @@
 
 (declaim (ftype (function (linked-list t) linked-list) add-to-front))
 (defun add-to-front (linked-list value)
+  "Add the value to the front of the linked list."
   (with-slots (head length) linked-list
     (let ((new-node (make-instance 'node :value value :previous-node head)))
       (setf (next-node head) new-node)
@@ -104,6 +133,7 @@
 
 (declaim (ftype (function (linked-list t) linked-list) add-to-rear))
 (defun add-to-rear (linked-list value)
+  "Add the value to the rear of the linked list."
   (with-slots (tail length) linked-list
     (let ((new-node (make-instance 'node :value value :next-node tail)))
       (setf (previous-node tail) new-node)
@@ -113,6 +143,7 @@
 
 (declaim (ftype (function (linked-list t) linked-list) add-to-empty))
 (defun add-to-empty (linked-list value)
+  "Add the value to an empty list."
   (with-slots (head tail length) linked-list
     (let ((new-node (make-instance 'node :value value)))
       (setf head new-node)
@@ -122,9 +153,10 @@
 
 (declaim (ftype (function (linked-list) t) remove-from-single-item-list))
 (defun remove-from-single-item-list (linked-list)
+  "Remove the value from a list containing only one item."
   (with-slots (head tail length) linked-list
     (let ((return-value (value head)))
-      (nil-node head)
+      (reset-node head)
       (setf head nil)
       (setf tail nil)
       (setf length 0)
@@ -132,26 +164,29 @@
 
 (declaim (ftype (function (linked-list) t) remove-from-front))
 (defun remove-from-front (linked-list)
+  "Remove the value from the front of the list."
   (with-slots (head length) linked-list
     (let ((return-value (value head))
 	  (new-head (previous-node head)))
-      (nil-node head)
+      (reset-node head)
       (setf head new-head)
       (setf length (1- length))
       return-value)))
 
 (declaim (ftype (function (linked-list) t) remove-from-rear))
 (defun remove-from-rear (linked-list)
+  "Remove the value from the rear of the list."
   (with-slots (tail length) linked-list
     (let ((return-value (value tail))
 	  (new-tail (next-node tail)))
-      (nil-node tail)
+      (reset-node tail)
       (setf tail new-tail)
       (setf length (1- length))
       return-value)))
 
 (declaim (ftype (function (node)) nil-node))
-(defun nil-node (node)
+(defun reset-node (node)
+  "Reset the node so that it points to nothing and contains nothing."
   (with-slots (value next-node previous-node) node
     (setf value nil)
     (setf next-node nil)
