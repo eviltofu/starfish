7af37a89b3957fb25f8379908c9642243d9183e7
Jerome Chan <eviltofu@mac.com>
2020-06-24 14:03:55 +0800

    Update time.lisp
    Added documentation strings.

--- src/time.lisp
+++ src/time.lisp
@@ -1,14 +1,19 @@
+;;;; Functions that handle time.
+
 (in-package #:starfish)
 
 
 (declaim (type (non-negative-integer-type) +days-in-seconds+))
-(defconstant +days-in-seconds+ 86400)
+(defconstant +days-in-seconds+ 86400
+  "Number of seconds in a day.")
 
 (declaim (type (non-negative-integer-type) +hours-in-seconds+))
-(defconstant +hours-in-seconds+ 3600)
+(defconstant +hours-in-seconds+ 3600
+  "Number of seconds in an hour.")
 
 (declaim (type (non-negative-integer-type) minutes-in-seconds))
-(defconstant +minutes-in-seconds+ 60)
+(defconstant +minutes-in-seconds+ 60
+  "Number of seconds in a minute.")
 
 (declaim
  (ftype (function
@@ -18,6 +23,7 @@
 	       (:seconds non-negative-integer-type)) non-negative-integer-type)
 	time-interval-in-seconds))
 (defun time-interval-in-seconds (&key (days 0) (hours 0) (minutes 0) (seconds 0))
+  "Create a time interval given days, hours, minutes, and seconds for input."
   (+ seconds
      (minutes minutes)
      (hours hours)
@@ -32,6 +38,7 @@
 	  (:seconds non-negative-integer-type)) non-negative-integer-type)
 	offset-from-time))
 (defun offset-from-time (time &key (days 0) (hours 0) (minutes 0) (seconds 0))
+  "Compute the new time given a time and an offset."
   (+ time
      (time-interval-in-seconds :days days
 			       :hours hours
@@ -46,6 +53,7 @@
 	       (:seconds non-negative-integer-type)) non-negative-integer-type)
 	offset-from-now))
 (defun offset-from-now (&key (days 0) (hours 0) (minutes 0) (seconds 0))
+  "Compute the new time from now and an offset."
   (offset-from-time (get-universal-time)
 		    :days days
 		    :hours hours
@@ -54,16 +62,20 @@
 
 (declaim (ftype (function (non-negative-integer-type) non-negative-integer-type) days))
 (defun days (var)
+  "Convert days into seconds."
   (* var +days-in-seconds+))
 
 (declaim (ftype (function (non-negative-integer-type) non-negative-integer-type) hours))
 (defun hours (var)
+  "Convert hours into seconds."
   (* var +hours-in-seconds+))
 
 (declaim (ftype (function (non-negative-integer-type) non-negative-integer-type) minutes))
 (defun minutes (var)
+  "Convert minutes into seconds."
   (* var +minutes-in-seconds+))
 
 (declaim (ftype (function (non-negative-integer-type) non-negative-integer-type) seconds))
 (defun seconds (var)
+  "Number of seconds. Use as a mnemonic device."
   var)
